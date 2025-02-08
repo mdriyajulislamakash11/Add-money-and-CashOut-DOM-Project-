8881b8b6538b87ba437400b448c0edf0ed63a27d
mdriyajulislamakash11 <hafejmohammadakash@gmail.com>
2025-02-09 01:11:36 +0600

    payment mathods doe

---
+++ future.js
@@ -0,0 +1,11 @@
+document.getElementById("show-add-money-form").addEventListener("click", function(){
+      document.getElementById("add-money-form").classList.remove("hidden");
+
+      document.getElementById("cash-out-form").classList.add("hidden")
+})
+
+document.getElementById("show-cash-out-form").addEventListener("click", function(){
+      document.getElementById("cash-out-form").classList.remove("hidden");
+
+      document.getElementById("add-money-form").classList.add("hidden")
+})
